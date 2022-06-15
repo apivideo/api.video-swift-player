cb72a596c6c30bfdd665da7839df8102f328e116
romain petit <contact.romain.petit@gmail.com>
2022-06-15 14:58:08 +0200

    chore() add documentation

--- Sources/ApiVideoPlayer/View/PlayerSwiftUIView.swift
+++ Sources/ApiVideoPlayer/View/PlayerSwiftUIView.swift
@@ -8,21 +8,30 @@
 import SwiftUI
 import AVKit
 
-@available(iOS 13.0, *)
+@available(iOS 14.0, *)
 @available(tvOS 13.0, *)
+/// Player for SwuiftUI
 public struct PlayerSwiftUIView: UIViewRepresentable {
+    @State private var fullScreen = false
+    
     let videoId: String!
     let videoType: VideoType!
     let events: PlayerEvents!
-    var playerView: PlayerView?
+    var playerView: ApiPlayerView?
     
-    public init(videoId : String, videoType: VideoType, events: PlayerEvents){
+    /// Init method for the player
+    /// - Parameters:
+    ///   - videoId: Need videoid to display the video
+    ///   - videoType: VideoType object to display vod or live controls
+    ///   - events: Callback to get all the player events
+    ///   - privateToken: Use only if your video is private, is nil by default
+    public init(videoId : String, videoType: VideoType, events: PlayerEvents, privateToken: String? = nil){
         self.videoId = videoId
         self.videoType = videoType
         self.events = events
         
         do{
-            playerView = try PlayerView(frame: .zero, videoId: videoId, videoType: videoType, events: events)
+            playerView = try ApiPlayerView(frame: .zero, videoId: videoId, videoType: videoType, events: events, privateToken: privateToken)
         }catch{
             playerView = nil
         }
@@ -36,61 +45,94 @@ public struct PlayerSwiftUIView: UIViewRepresentable {
     }
     
     public func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<PlayerSwiftUIView>) {
-        
+        print("view upated")
     }
+    
+
+    
+    /// Pause the video
     public func pause(){
         playerView?.pause()
     }
+    /// Play the video
     public func play(){
         playerView?.play()
-        
     }
+    /// Replay the video
     public func replay(){
         playerView?.replay()
     }
+    /// Get the current time of the video
+    /// - Returns: Current time in CMTime
     public func getCurrentTime() -> CMTime{
        return playerView!.getCurrentTime()
     }
+    /// Mute the playing video
     public func mute(){
         playerView?.mute()
     }
+    /// Unmute the playing video
     public func unMute(){
         playerView?.unMute()
     }
+    /// Get audio state of the player
+    /// - Returns: Boolean of the state of player's audio
     public func isMuted()->Bool{
         return ((playerView?.isMuted()) != nil)
     }
+    /// Hide all the controls of the player
+    /// By default the controls are on. They will be hide in case of inactivity, and display again on user interaction.
     public func hideControls(){
         playerView?.hideControls()
     }
+    /// Video player is looping.
+    /// (When the video play is finished, the player will start again the video)
     public func setLoop(){
         playerView?.setLoop()
     }
+    /// Stop video looping
     public func stopLooping(){
         playerView?.stopLooping()
     }
+    /// Go forward or backward in the video
+    /// - Parameter time: time in seconds, (use minus to go backward)
     public func seek(time: Double){
         playerView?.seek(time: time)
     }
+    /// The video player volume is connected to the device audio volume
+    /// - Parameter volume: Float between 0 to 1 
     public func setVolume(volume: Float){
         playerView?.setVolume(volume: volume)
     }
-    public func getDuration(completion: @escaping (CMTime) -> ()){
-        playerView?.getDuration(){ (duration) in
-            completion(duration)
-        }
-    }
+//    /// Get the duration of the video
+//    /// - Parameter completion: 
+//    public func getDuration(completion: @escaping (CMTime) -> ()){
+//        playerView?.getDuration(){ (duration) in
+//            completion(duration)
+//        }
+//    }
     
     
+    /// Display the player in fullscreen
     public func goFullScreen(){
-        playerView?.goFullScreen()
+        let controller = AVPlayerViewController()
+        controller.player = playerView!.avPlayer
+        
+        self.fullScreen.toggle()
+        print(fullScreen.description)
     }
 }
 
-@available(iOS 13.0, *)
+@available(iOS 14.0, *)
 @available(tvOS 13.0.0, *)
 struct SwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
-        PlayerSwiftUIView(videoId: "toto", videoType: .vod, events: PlayerEvents())
+        PlayerSwiftUIView(videoId: "vi3wxypv6quTSFwXvg5XJ5az", videoType: .vod, events: PlayerEvents())
     }
+}
+
+extension UIScreen{
+   static let screenWidth = UIScreen.main.bounds.size.width
+   static let screenHeight = UIScreen.main.bounds.size.height
+   static let screenSize = UIScreen.main.bounds.size
 }
