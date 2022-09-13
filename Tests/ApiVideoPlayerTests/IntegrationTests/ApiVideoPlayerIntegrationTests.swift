7b6bb5ebd839515ad171d33683ff9af2ed6fe03a
romain petit <contact.romain.petit@gmail.com>
2022-09-13 15:15:44 +0200

    feat() add unit tests and integration tests

--- Tests/ApiVideoPlayerTests/IntegrationTests/ApiVideoPlayerIntegrationTests.swift
+++ Tests/ApiVideoPlayerTests/IntegrationTests/ApiVideoPlayerIntegrationTests.swift
@@ -1,5 +1,7 @@
 @testable import ApiVideoPlayer
 import XCTest
+import CoreMedia
+
 
 /// Integration tests with connection to api.video
 @available(iOS 14.0, *)
@@ -10,18 +12,114 @@ final class ApiVideoPlayerIntegrationTests: XCTestCase {
     /// Assert that a valid video id is correctly played
     /// Check that the PlayerEvents are correctly called: didPrepare, didPlay, didEnd,...
     func testValidVideoId() throws {
-        // TODO:
+        let completedExpectationPrepare = expectation(description: "Completed Prepare")
+        let completedExpectationPlay = expectation(description: "Completed Play")
+        let completedExpectationPause = expectation(description: "Completed Pause")
+        let completedExpectationSeek = expectation(description: "Completed Seek")
+        let completedExpectationEnd = expectation(description: "Completed End")
+        var didPrepare = false
+        var didPlay = false
+        var didPause = false
+        var didSeek = false
+        var didEnd = false
+        let events = PlayerEvents(
+            didPrepare: {() in
+                print("ready")
+                didPrepare = true
+                completedExpectationPrepare.fulfill()
+            },
+            didPause: {() in
+                print("paused test")
+                if(!didPause){
+                    didPause = true
+                    completedExpectationPause.fulfill()
+                }
+            },
+            didPlay: {() in
+                print("play")
+                if(!didPlay){
+                    didPlay = true
+                    completedExpectationPlay.fulfill()
+                }
+            },
+            didReplay: {() in
+                print("video replayed")
+            },
+            didLoop: {() in
+                print("video replayed from loop")
+            },
+            didSetVolume: {(volume) in
+                print("volume set to : \(volume)")
+            },
+            didSeek: {(from, to)in
+                print("seek from : \(from), to: \(to)")
+                didSeek = true
+                completedExpectationSeek.fulfill()
+            },
+            didEnd: {() in
+                print("Ended")
+                didEnd = true
+                completedExpectationEnd.fulfill()
+            }
+        )
+        let playerView = ApiVideoPlayerView(frame: .zero, videoId: validVideoId, videoType: VideoType.vod /* only .vod is supported */, events: events)
+        wait(for: [completedExpectationPrepare], timeout: 10)
+        playerView.play()
+        wait(for: [completedExpectationPlay], timeout: 3)
+        playerView.pause()
+        wait(for: [completedExpectationPause], timeout: 3)
+        playerView.seek(to: CMTime(seconds: 59.0, preferredTimescale: 100))
+        wait(for: [completedExpectationSeek], timeout: 3)
+        playerView.play()
+        wait(for: [completedExpectationEnd], timeout: 9)
+        
+        XCTAssertTrue(didEnd)
+        XCTAssertTrue(didSeek)
+        XCTAssertTrue(didPause)
+        XCTAssertTrue(didPlay)
+        XCTAssertTrue(didPrepare)
+
     }
 
     /// Assert that the duration of the video is the expected duration
     /// Retrieve the validVideoId video and check its duration
     func testDuration() throws {
-        let duration: Double = 0 // TODO:
+        let completedExpectation = expectation(description: "Completed")
+        let events = PlayerEvents(
+            didPrepare: {() in
+                print("ready")
+                completedExpectation.fulfill()
+            },
+            didError: {(error) in
+                print("error : \(error)")
+                completedExpectation.fulfill()
+                XCTFail("should work")
+            }
+        )
+        var duration: Double = 0
+        let playerView = ApiVideoPlayerView(frame: .zero, videoId: validVideoId, videoType: VideoType.vod /* only .vod is supported */, events: events)
+        waitForExpectations(timeout: 10, handler: nil)
+        duration = playerView.duration.seconds
         XCTAssertEqual(duration, 60.2)
+        
     }
 
     /// Assert that didError is triggered when an invalid video id is passed
     func testInvalidVideoId() throws {
-        // TODO:
+        let completedExpectation = expectation(description: "Completed")
+        let events = PlayerEvents(
+            didPrepare: {() in
+                print("ready")
+                XCTFail("Should return didError")
+            },
+            didError: {(error) in
+                print("error : \(error)")
+                completedExpectation.fulfill()
+                XCTAssertNotNil(error, "error should not be nil")
+            }
+        )
+        let playerView = ApiVideoPlayerView(frame: .zero, videoId: invalidVideoId, videoType: VideoType.vod /* only .vod is supported */, events: events)
+        //playerView.play()
+        waitForExpectations(timeout: 10, handler: nil)
     }
 }
--- Tests/ApiVideoPlayerTests/UnitTests/ApiVideoPlayerUnitTests.swift
+++ Tests/ApiVideoPlayerTests/UnitTests/ApiVideoPlayerUnitTests.swift
@@ -1,25 +1,79 @@
-import ApiVideoPlayer
+@testable import ApiVideoPlayer
 import XCTest
 
 /// Unit tests on PlayerController without connection to api.video
 /// The connection is mocked with MockedTasksExecutor
 @available(iOS 14.0, *)
 class ApiVideoPlayerUnitTests: XCTestCase {
+    
     /// Assert that didError is not called if the JSON is valid
     func testWithValidPlayerManifestJson() throws {
-        // MockedTasksExecutor.data = // TODO read valid Player manifest json from responseSuccess
-        // let playerController = ApiVideoPlayerController(..., taskExecutor: MockedTasksExecutor)
+        let events = PlayerEvents(
+            didPrepare: { () in
+                print("didPrepare")
+                XCTAssertTrue(true)
+            },
+            didError: { error in
+                print("error \(error)")
+                XCTFail("Error should success")
+            }
+        )
+        
+        guard let resourceUrl = Bundle.module.url(forResource: "responseSuccess",withExtension: "json") else {
+            XCTFail("Error can't find the json file")
+            return
+        }
+        do {
+            let data = try Data(contentsOf: resourceUrl, options: .mappedIfSafe)
+            MockedTasksExecutor.data = data
+        }catch{
+            XCTFail("Error can't get data from json")
+        }
+        let _ = ApiVideoPlayerController(videoId: "vi18RL1kvZlDRdzk7Mas59HT", videoType: .vod, events: events, taskExecutor: MockedTasksExecutor.self)
     }
 
     /// Assert didError is called if the JSON is invalid (syntax error or missing values)
     func testWithInvalidPlayerManifestJson() throws {
-        // MockedTasksExecutor.data = // TODO invalid Player manifest json
-        // let playerController = ApiVideoPlayerController(..., taskExecutor: MockedTasksExecutor)
+        let events = PlayerEvents(
+            didPrepare: { () in
+                print("didPrepare")
+                XCTFail("Should get an error")
+            },
+            didError: { error in
+                print("error toto \(error)")
+                XCTAssertTrue(true)
+            }
+        )
+        guard let resourceUrl = Bundle.module.url(forResource: "responseError",withExtension: "json") else {
+            XCTFail("Error can't find the json file")
+            return
+        }
+        do {
+            let data = try Data(contentsOf: resourceUrl, options: .mappedIfSafe)
+            MockedTasksExecutor.data = data
+        }catch{
+            XCTFail("Error can't get data from json")
+        }
+        let _ = ApiVideoPlayerController(videoId: "vi18RL1kvZlDRdzk7Mas59HT", videoType: .vod, events: events, taskExecutor: MockedTasksExecutor.self)
     }
 
     /// Assert didError is called if the server returns an error
     func testWithServerError() throws {
-        // MockedTasksExecutor.error = // TODO error
-        // let playerController = ApiVideoPlayerController(..., taskExecutor: MockedTasksExecutor)
+        MockedTasksExecutor.error = MockServerError.serverError("error 500")// TODO error
+        let events = PlayerEvents(
+            didPrepare: { () in
+                print("didPrepare")
+                XCTFail("Should get an error")
+            },
+            didError: { error in
+                print("error toto \(error)")
+                XCTAssertTrue(true)
+            }
+        )
+        let _ = ApiVideoPlayerController(videoId: "vi18RL1kvZlDRdzk7Mas59HT", videoType: .vod, events: events, taskExecutor: MockedTasksExecutor.self)
     }
+}
+
+enum MockServerError: Error {
+    case serverError(String)
 }
