60d6e5637b28e7c73791c575efd0af1ba80d6c19
romain petit <contact.romain.petit@gmail.com>
2022-10-03 18:47:42 +0200

    feat() add tests for the PlayerController

---
+++ Tests/ApiVideoPlayerTests/IntegrationTests/ApiVideoPlayerControllerIntegrationTests.swift
@@ -0,0 +1,94 @@
+@testable import ApiVideoPlayer
+import CoreMedia
+import XCTest
+
+@available(iOS 14.0, *)
+final class ApiVideoPlayerControllerIntegrationTests: XCTestCase {
+  private let validVideoId = "vi2G6Qr8ZVE67dWLNymk7qbc"
+  private let invalidVideoId = "unknownVideoId"
+
+  func testValidVideoIdPlay() throws {
+    let expectation = self.expectation(description: "delegate is called 2 times")
+    expectation.expectedFulfillmentCount = 2
+
+    let events = PlayerEvents(
+      didPrepare: { () in
+        print("ready")
+        expectation.fulfill()
+      },
+      didPlay: { () in
+        print("play")
+        expectation.fulfill()
+      }
+    )
+    let controller = ApiVideoPlayerController(videoId: validVideoId, videoType: .vod, events: events)
+    controller.play()
+    waitForExpectations(timeout: 10, handler: nil)
+  }
+
+  func testValidVideoIdPause() throws {
+    let completedExpectationPrepare = expectation(description: "Completed Prepare")
+    let completedExpectationPlay = expectation(description: "Completed Play")
+    let completedExpectationPause = expectation(description: "Completed Pause")
+    let events = PlayerEvents(
+      didPrepare: { () in
+        print("ready")
+        completedExpectationPrepare.fulfill()
+      },
+      didPause: { () in
+        print("paused test")
+        completedExpectationPause.fulfill()
+      },
+      didPlay: { () in
+        print("play")
+        completedExpectationPlay.fulfill()
+      }
+    )
+    let controller = ApiVideoPlayerController(videoId: validVideoId, videoType: .vod, events: events)
+    wait(for: [completedExpectationPrepare], timeout: 10)
+    controller.play()
+    wait(for: [completedExpectationPlay], timeout: 2)
+    controller.pause()
+    wait(for: [completedExpectationPause], timeout: 2)
+
+  }
+
+  func testDuration() throws {
+    let expectation = self.expectation(description: "delegate is called 3 times")
+    expectation.expectedFulfillmentCount = 1
+    let events = PlayerEvents(
+      didPrepare: { () in
+        print("ready")
+        expectation.fulfill()
+      },
+      didError: { error in
+        print("error : \(error)")
+        expectation.fulfill()
+        XCTFail("\(error)")
+      }
+    )
+    var duration: Double = 0
+    let controller = ApiVideoPlayerController(videoId: validVideoId, videoType: .vod, events: events)
+    waitForExpectations(timeout: 10, handler: nil)
+    duration = controller.duration.seconds
+    XCTAssertEqual(duration, 60.2)
+  }
+
+  func testInvalidVideoId() throws {
+    let completedExpectation = expectation(description: "Completed")
+    let events = PlayerEvents(
+      didPrepare: { () in
+        print("ready")
+        XCTFail("Should return didError")
+      },
+      didError: { error in
+        print("error : \(error)")
+        completedExpectation.fulfill()
+        XCTAssertNotNil(error, "\(error)")
+      }
+    )
+    _ = ApiVideoPlayerController(videoId: self.invalidVideoId, videoType: .vod, events: events)
+    waitForExpectations(timeout: 10, handler: nil)
+  }
+
+}
