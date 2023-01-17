a3385ab744d67e4233dc99e178360a46006e1ab7
Romain Petit <contact.romain.petit@gmail.com>
2023-01-17 14:59:55 +0100

    feat() add live indicator, current time display

--- Sources/ApiVideoPlayer/View/ControlsView.swift
+++ Sources/ApiVideoPlayer/View/ControlsView.swift
@@ -3,7 +3,7 @@ import AVFoundation
 import Foundation
 import UIKit
 
-class ControlsView: UIView, UIGestureRecognizerDelegate{
+class ControlsView: UIView, UIGestureRecognizerDelegate {
     private let playerController: ApiVideoPlayerController
     private let videoOptions: VideoOptions
 
@@ -23,7 +23,7 @@ class ControlsView: UIView, UIGestureRecognizerDelegate{
     private let events = PlayerEvents()
     private var timerLeadingConstraintWithSubtitleButton: NSLayoutConstraint?
     private var timerLeadingConstraintWithoutSubtitleButton: NSLayoutConstraint?
-    
+
     init(frame: CGRect, playerController: ApiVideoPlayerController, videoOptions: VideoOptions) {
         self.playerController = playerController
         self.videoOptions = videoOptions
@@ -31,24 +31,24 @@ class ControlsView: UIView, UIGestureRecognizerDelegate{
         playerController.setTimerObserver(callback: { () in
             self.updateTiming()
         })
-        setGenericButtons()
-        if(videoOptions.videoType == .vod){
-            setVodView()
-        }else{
-            setLiveView()
+        self.setGenericButtons()
+        if videoOptions.videoType == .vod {
+            self.setVodView()
+        } else {
+            self.setLiveView()
         }
-        
-        setupGeneralEvents()
-        
+
+        self.setupGeneralEvents()
+
         playerController.addEvents(events: self.events)
     }
-    
+
     @available(*, unavailable)
     required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    private func setGenericButtons(){
+
+    private func setGenericButtons() {
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.handleTap))
         tapGestureRecognizer.delegate = self
         addGestureRecognizer(tapGestureRecognizer)
@@ -58,7 +58,7 @@ class ControlsView: UIView, UIGestureRecognizerDelegate{
         addSubview(self.playPauseButton)
         self.playPauseButton.addTarget(self, action: #selector(self.playPauseAction), for: .touchUpInside)
         self.setPlayBtnIcon(iconName: "play-primary")
-        
+
         // Slider View
         addSubview(self.controlSliderView)
         self.controlSliderView.backgroundColor = UIColor.darkGray.withAlphaComponent(0.25)
@@ -72,16 +72,15 @@ class ControlsView: UIView, UIGestureRecognizerDelegate{
         )
         self.controlSlider.tintColor = UIColor.orange.withAlphaComponent(0.7)
         self.controlSlider.thumbTintColor = UIColor.white
-        
-        
+
         addSubview(self.fullScreenButton)
         self.fullScreenButton.addTarget(self, action: #selector(self.goToFullScreenAction), for: .touchUpInside)
         self.fullScreenButton.isHidden = true
-        
-        setupGeneralConstraints()
+
+        self.setupGeneralConstraints()
     }
-    
-    private func setupGeneralConstraints(){
+
+    private func setupGeneralConstraints() {
         // Play Pause Button
         self.playPauseButton.translatesAutoresizingMaskIntoConstraints = false
         self.playPauseButton.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
@@ -95,7 +94,6 @@ class ControlsView: UIView, UIGestureRecognizerDelegate{
         self.controlSliderView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
         self.controlSliderView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
 
-        
         // Slider
         self.controlSlider.translatesAutoresizingMaskIntoConstraints = false
         self.controlSlider.centerYAnchor.constraint(equalTo: self.controlSliderView.centerYAnchor)
@@ -104,8 +102,7 @@ class ControlsView: UIView, UIGestureRecognizerDelegate{
             equalTo: self.controlSliderView.leadingAnchor,
             constant: 10
         ).isActive = true
-        
-        
+
         // FullScreen Button
         self.fullScreenButton.translatesAutoresizingMaskIntoConstraints = false
         self.fullScreenButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10).isActive = true
@@ -113,8 +110,8 @@ class ControlsView: UIView, UIGestureRecognizerDelegate{
         self.fullScreenButton.widthAnchor.constraint(equalToConstant: 70).isActive = true
         self.fullScreenButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
     }
-    
-    private func setupVodConstraints(){
+
+    private func setupVodConstraints() {
         self.controlSliderView.heightAnchor.constraint(equalToConstant: 50).isActive = true
 
         // Go Forward Button
@@ -136,17 +133,16 @@ class ControlsView: UIView, UIGestureRecognizerDelegate{
         ).isActive = true
         self.vodControlGoBackward15Button.widthAnchor.constraint(equalToConstant: 70).isActive = true
         self.vodControlGoBackward15Button.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        
+
         // Slider right anchor
         self.controlSlider.rightAnchor.constraint(equalTo: self.controlTimerLabel.leftAnchor, constant: -10)
             .isActive = true
-        
-        
+
         // Timer Label
         self.controlTimerLabel.translatesAutoresizingMaskIntoConstraints = false
         self.controlTimerLabel.centerYAnchor.constraint(equalTo: self.controlSliderView.centerYAnchor)
             .isActive = true
-        
+
         self.timerLeadingConstraintWithSubtitleButton = self.controlTimerLabel.trailingAnchor.constraint(
             equalTo: self.subtitleButton.leadingAnchor,
             constant: -10
@@ -156,8 +152,8 @@ class ControlsView: UIView, UIGestureRecognizerDelegate{
             constant: -10
         )
         self.timerLeadingConstraintWithoutSubtitleButton?.isActive = true
-        
-        //subtitle
+
+        // subtitle
         self.subtitleButton.translatesAutoresizingMaskIntoConstraints = false
         self.subtitleButton.centerYAnchor.constraint(equalTo: self.controlSliderView.centerYAnchor)
             .isActive = true
@@ -165,59 +161,59 @@ class ControlsView: UIView, UIGestureRecognizerDelegate{
             equalTo: self.controlSliderView.trailingAnchor,
             constant: -10
         ).isActive = true
-        
+
     }
-    
-    private func setupLiveConstraints(){
+
+    private func setupLiveConstraints() {
         // sliderview height
         self.controlSliderView.heightAnchor.constraint(equalToConstant: 60).isActive = true
 
         // Slider right anchor
         self.controlSlider.rightAnchor.constraint(equalTo: self.liveButton.leftAnchor, constant: -10)
             .isActive = true
-        
+
         self.liveButton.translatesAutoresizingMaskIntoConstraints = false
         self.liveButton.centerYAnchor.constraint(equalTo: self.controlSliderView.centerYAnchor)
             .isActive = true
         self.liveButton.rightAnchor.constraint(
             equalTo: self.controlSliderView.rightAnchor,
-            constant: -10).isActive = true
-        
+            constant: -10
+        ).isActive = true
+
         self.liveButton.widthAnchor.constraint(equalToConstant: 40).isActive = true
         self.liveButton.heightAnchor.constraint(equalToConstant: 25).isActive = true
-        
-        
-        self.controlLiveDurationTimerLabel.translatesAutoresizingMaskIntoConstraints = false
-        self.controlLiveDurationTimerLabel.topAnchor.constraint(equalTo: self.controlSlider.bottomAnchor).isActive = true
-        self.controlLiveDurationTimerLabel.leadingAnchor.constraint(equalTo: self.controlSliderView.leadingAnchor, constant: 10).isActive = true
-        
-        self.controlCurrentTimeFromLiveLabel.translatesAutoresizingMaskIntoConstraints = false
-        self.controlCurrentTimeFromLiveLabel.centerXAnchor.constraint(equalTo: self.liveButton.centerXAnchor).isActive = true
-        self.controlCurrentTimeFromLiveLabel.topAnchor.constraint(equalTo: self.liveButton.bottomAnchor).isActive = true
-        
+
+        self.controlLiveCurrentTimerLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.controlLiveCurrentTimerLabel.topAnchor.constraint(equalTo: self.controlSlider.bottomAnchor)
+            .isActive = true
+        self.controlLiveCurrentTimerLabel.leadingAnchor.constraint(
+            equalTo: self.controlSliderView.leadingAnchor,
+            constant: 10
+        ).isActive = true
+
+        self.controlTimeToLiveLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.controlTimeToLiveLabel.centerXAnchor.constraint(equalTo: self.liveButton.centerXAnchor)
+            .isActive = true
+        self.controlTimeToLiveLabel.topAnchor.constraint(equalTo: self.liveButton.bottomAnchor).isActive = true
+
     }
-    
-    private func setLiveView(){
-        print("LIVE VIEW")
+
+    private func setLiveView() {
         self.controlSliderView.addSubview(self.liveButton)
-        self.controlSliderView.addSubview(self.controlLiveDurationTimerLabel)
-        self.controlSliderView.addSubview(self.controlCurrentTimeFromLiveLabel)
-        
+        self.controlSliderView.addSubview(self.controlLiveCurrentTimerLabel)
+        self.controlSliderView.addSubview(self.controlTimeToLiveLabel)
+
         self.liveButton.addTarget(self, action: #selector(self.goToLive), for: .touchUpInside)
-        //TODO: Cacher thumb slider pour le rendre plus jolie
-//        self.controlSlider.setThumbImage(UIImage(), for: .normal)
-        
-        self.controlLiveDurationTimerLabel.text = "00:00"
-        self.controlCurrentTimeFromLiveLabel.text = "00:00"
-        
-        self.controlLiveDurationTimerLabel.font = self.controlLiveDurationTimerLabel.font.withSize(10)
-        self.controlCurrentTimeFromLiveLabel.font = self.controlCurrentTimeFromLiveLabel.font.withSize(10)
-        setupLiveConstraints()
+        self.controlLiveCurrentTimerLabel.text = "00:00"
+        self.controlTimeToLiveLabel.text = "00:00"
+
+        self.controlLiveCurrentTimerLabel.font = self.controlLiveCurrentTimerLabel.font.withSize(10)
+        self.controlTimeToLiveLabel.font = self.controlTimeToLiveLabel.font.withSize(10)
+        self.setupLiveConstraints()
     }
-    
-    private func setVodView(){
-        print("VOD VIEW")
-        setupVodEvents()
+
+    private func setVodView() {
+        self.setupVodEvents()
         // Go Forward Button
         addSubview(self.vodControlGoForward15Button)
         self.vodControlGoForward15Button.addTarget(
@@ -233,27 +229,26 @@ class ControlsView: UIView, UIGestureRecognizerDelegate{
             action: #selector(self.goBackward15Action),
             for: .touchUpInside
         )
-        
+
         // Subtitle
         self.controlSliderView.addSubview(self.subtitleButton)
         self.subtitleButton.addTarget(self, action: #selector(self.toggleSubtitleView), for: .touchUpInside)
-        
+
         // Timer Label
         self.controlSliderView.addSubview(self.controlTimerLabel)
         self.controlTimerLabel.textColor = UIColor.white
 
-
-        setupVodConstraints()
+        self.setupVodConstraints()
     }
-    
-    private func setupGeneralEvents(){
+
+    private func setupGeneralEvents() {
         self.events.didPlay = { () in
             self.setPlayBtnIcon(iconName: "pause-primary")
         }
         self.events.didPause = { () in
             self.setPlayBtnIcon(iconName: "play-primary")
         }
-        
+
         self.events.didPrepare = { () in
             if self.playerController.hasSubtitles {
                 DispatchQueue.main.async {
@@ -264,14 +259,13 @@ class ControlsView: UIView, UIGestureRecognizerDelegate{
             }
         }
     }
-    
-    private func setupVodEvents(){
+
+    private func setupVodEvents() {
         self.events.didEnd = { () in
             self.setPlayBtnIcon(iconName: "replay-primary")
         }
     }
-    
-    
+
     let playPauseButton: UIButton = {
         let btn = UIButton(type: .system)
         btn.tintColor = .white
@@ -280,7 +274,7 @@ class ControlsView: UIView, UIGestureRecognizerDelegate{
         btn.imageEdgeInsets = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
         return btn
     }()
-    
+
     let vodControlGoForward15Button: UIButton = {
         let btn = UIButton(type: .system)
         btn.setImage(UIImage(systemName: "goforward.15"), for: .normal)
@@ -294,37 +288,40 @@ class ControlsView: UIView, UIGestureRecognizerDelegate{
         btn.tintColor = .white
         return btn
     }()
-    
+
     let fullScreenButton: UIButton = {
         let btn = UIButton(type: .system)
         btn.setImage(UIImage(systemName: "arrow.up.left.and.arrow.down.right"), for: .normal)
         btn.tintColor = .white
         return btn
     }()
+
     let controlSliderView: UIView = {
         let view = UIView()
         return view
     }()
+
     let controlSlider: UISlider = {
         let slider = UISlider()
         return slider
     }()
-    
+
     let controlTimerLabel: UILabel = {
         let label = UILabel()
         return label
     }()
-    
+
     //    Timers for live
-    let controlLiveDurationTimerLabel: UILabel = {
+    let controlLiveCurrentTimerLabel: UILabel = {
         let label = UILabel()
         return label
     }()
-    let controlCurrentTimeFromLiveLabel: UILabel = {
+
+    let controlTimeToLiveLabel: UILabel = {
         let label = UILabel()
         return label
     }()
-    
+
     let subtitleButton: UIButton = {
         let btn = UIButton(type: .system)
         btn.setImage(UIImage(systemName: "text.bubble"), for: .normal)
@@ -332,14 +329,14 @@ class ControlsView: UIView, UIGestureRecognizerDelegate{
         btn.isHidden = true
         return btn
     }()
-    
+
     let liveButton: UIButton = {
         let btn = UIButton(type: .system)
         btn.tintColor = .white
         btn.contentHorizontalAlignment = .fill
         btn.contentVerticalAlignment = .fill
         btn.imageEdgeInsets = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
-        
+
         if #available(tvOS 13.0, *) {
             btn.setImage(
                 UIImage(named: "live-primary", in: ApiVideoPlayerResources.resourceBundle, compatibleWith: nil),
@@ -350,7 +347,7 @@ class ControlsView: UIView, UIGestureRecognizerDelegate{
         }
         return btn
     }()
-    
+
     private func setPlayBtnIcon(iconName: String) {
         if #available(tvOS 13.0, *) {
             playPauseButton.setImage(
@@ -361,12 +358,12 @@ class ControlsView: UIView, UIGestureRecognizerDelegate{
             // Fallback on earlier versions
         }
     }
-    
+
     private func resetTimer() {
         self.timer?.invalidate()
         self.timer = nil
     }
-    
+
     private func activateTimer() {
         guard self.timer == nil else { return }
         self.timer = Timer.scheduledTimer(
@@ -377,8 +374,7 @@ class ControlsView: UIView, UIGestureRecognizerDelegate{
             repeats: false
         )
     }
-    
-    
+
     @objc
     func handleTap(_: UIGestureRecognizer) {
         self.resetTimer()
@@ -387,7 +383,7 @@ class ControlsView: UIView, UIGestureRecognizerDelegate{
 
         self.subtitleView?.dismissView()
     }
-    
+
     @objc
     func playPauseAction() {
         self.resetTimer()
@@ -403,7 +399,7 @@ class ControlsView: UIView, UIGestureRecognizerDelegate{
         }
         self.activateTimer()
     }
-    
+
     @objc
     func goToFullScreenAction() {
         guard let vc = viewController else {
@@ -411,12 +407,12 @@ class ControlsView: UIView, UIGestureRecognizerDelegate{
         }
         self.playerController.goToFullScreen(viewController: vc)
     }
-    
+
     @objc
     func hideControlsHandler() {
         self.hideControls()
     }
-    
+
     @objc
     func goForward15Action() {
         self.resetTimer()
@@ -433,9 +429,9 @@ class ControlsView: UIView, UIGestureRecognizerDelegate{
         self.playerController.seek(offset: CMTime(seconds: -15, preferredTimescale: 1_000))
         self.activateTimer()
     }
-    
+
     private var sliderDidPauseVideo = false
-    
+
     @objc
     func playbackSliderValueChanged(slider: UISlider, event: UIEvent) {
         self.resetTimer()
@@ -468,7 +464,7 @@ class ControlsView: UIView, UIGestureRecognizerDelegate{
         }
         self.activateTimer()
     }
-    
+
     @objc
     func toggleSubtitleView() {
         self.resetTimer()
@@ -492,52 +488,65 @@ class ControlsView: UIView, UIGestureRecognizerDelegate{
         }
         self.activateTimer()
     }
-    
+
+    private func setLiveButtonTintColor(isLive: Bool) {
+        if isLive {
+            self.liveButton.tintColor = .red
+        } else {
+            self.liveButton.tintColor = .white
+        }
+    }
+
     @objc
     func goToLive() {
-        //TODO: finir comportement reset to live (seek vers le live)
-        self.liveButton.tintColor = .red
+        self.playerController.seek(to: self.playerController.duration)
+        self.setLiveButtonTintColor(isLive: true)
     }
-    
+
     private func showControls() {
         self.playPauseButton.isHidden = false
-        if(self.videoOptions.videoType == .vod){
+        if self.videoOptions.videoType == .vod {
             self.vodControlGoForward15Button.isHidden = false
             self.vodControlGoBackward15Button.isHidden = false
-        }else{
-            // faire le btn islive
         }
-        
         self.controlSliderView.isHidden = false
         self.fullScreenButton.isHidden = false
     }
-    
+
     private func hideControls() {
         self.playPauseButton.isHidden = true
-        if(self.videoOptions.videoType == .vod){
+        if self.videoOptions.videoType == .vod {
             self.vodControlGoForward15Button.isHidden = true
             self.vodControlGoBackward15Button.isHidden = true
-        }else{
-            // faire le btn islive
         }
         self.controlSliderView.isHidden = true
         self.fullScreenButton.isHidden = true
     }
-    
+
     private func updateTiming() {
         let currentTime = self.playerController.currentTime
         let duration = self.playerController.duration
         let remainingTime = duration - currentTime
 
-        self.controlSlider.value = Float(currentTime.roundedSeconds / duration.roundedSeconds)
-        if(self.videoOptions.videoType == .vod){
+        if self.videoOptions.videoType == .vod {
+            self.controlSlider.value = Float(currentTime.roundedSeconds / duration.roundedSeconds)
             self.controlTimerLabel.text = remainingTime.prettyTime
-        }else{
-            self.controlTimerLabel.text = "00:00"
+        } else {
+            var timeToLive = remainingTime.prettyTime
+            if remainingTime > CMTime(seconds: 0.0, preferredTimescale: 1_000) {
+                timeToLive = "-\(timeToLive)"
+            } else {
+                timeToLive = CMTime(seconds: 0.0, preferredTimescale: 1_000).prettyTime
+            }
+            if remainingTime >= CMTime(seconds: 3, preferredTimescale: 1_000) {
+                self.setLiveButtonTintColor(isLive: false)
+            } else {
+                self.setLiveButtonTintColor(isLive: true)
+            }
+            self.controlSlider.value = Float(currentTime.roundedSeconds / duration.roundedSeconds)
+            self.controlLiveCurrentTimerLabel.text = currentTime.prettyTime
+            self.controlTimeToLiveLabel.text = timeToLive
         }
-        
     }
-    
-
 }
 #endif
