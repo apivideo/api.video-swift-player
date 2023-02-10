abed9968746113fc5a53c7855349971c5c2bc6c5
Romain Petit <contact.romain.petit@gmail.com>
2023-02-10 17:11:31 +0100

    fix(swiftUI) add PlayerEventsDelegate heritance

--- Examples/ExampleSwiftUI/ExampleSwiftUI/ContentView.swift
+++ Examples/ExampleSwiftUI/ExampleSwiftUI/ContentView.swift
@@ -1,6 +1,7 @@
 import ApiVideoPlayer
+import CoreMedia
 import SwiftUI
-struct ContentView: View {
+struct ContentView: View, PlayerEventsDelegate {
     private var player: ApiVideoPlayer
     init() {
         self.player = ApiVideoPlayer(videoOptions: VideoOptions(videoId: "YOUR-VIDEO-ID"))
@@ -9,6 +10,9 @@ struct ContentView: View {
     var body: some View {
         VStack {
             player
+                .onAppear {
+                    self.player.addDelegate(delegate: self)
+                }
                 .frame(height: 250)
                 .padding(.bottom)
             HStack {
@@ -22,6 +26,56 @@ struct ContentView: View {
             Spacer()
         }
     }
+
+    public func didPrepare() {
+        print("app didPrepare")
+    }
+
+    public func didReady() {
+        print("app didReady")
+    }
+
+    public func didPause() {
+        print("app didPause")
+    }
+
+    public func didPlay() {
+        print("app didPlay")
+    }
+
+    public func didReplay() {
+        print("app didReplay")
+    }
+
+    public func didMute() {
+        print("app didMute")
+    }
+
+    public func didUnMute() {
+        print("app didUnMute")
+    }
+
+    public func didLoop() {
+        print("app didLoop")
+    }
+
+    public func didSetVolume(_: Float) {
+        print("app didSetVolume")
+    }
+
+    public func didSeek(_: CMTime, _: CMTime) {
+        print("app didSeek")
+    }
+
+    public func didEnd() {
+        print("app didEnd")
+    }
+
+    public func didError(_: Error) {
+        print("app didError")
+    }
+
+    public func didVideoSizeChanged(_: CGSize) {}
 }
 
 struct ContentView_Previews: PreviewProvider {
