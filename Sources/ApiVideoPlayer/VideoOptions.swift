c052e4c4e6102606e8f2d696caa343ab19900992
Romain Petit <contact.romain.petit@gmail.com>
2023-02-27 15:46:32 +0100

    feat() add new url of vod, and handle private url

--- Sources/ApiVideoPlayer/VideoOptions.swift
+++ Sources/ApiVideoPlayer/VideoOptions.swift
@@ -1,16 +1,56 @@
 import Foundation
 
 public struct VideoOptions {
-    public var videoId: String
-    public var videoType: VideoType
+    public let videoId: String
+    public let videoType: VideoType
+    public let token: String?
 
-    public init(videoId: String, videoType: VideoType) {
+    private var baseUrl = ""
+    private var vodUrl = ""
+    private var liveUrl = ""
+
+    public init(videoId: String, videoType: VideoType, token: String? = nil) {
         self.videoId = videoId
         self.videoType = videoType
+        self.token = token
+
+        baseUrl = "\(videoType.rawValue)/\(videoId)"
+        vodUrl = baseUrl
+        liveUrl = "\(videoType.rawValue)"
+        if let token = token {
+            vodUrl.append("/token/\(token)")
+            liveUrl.append("/private/\(token)")
+        }
+        liveUrl.append("/\(videoId)")
     }
+
+    public var hlsManifestUrl: String {
+        if videoType == .vod {
+            return "\(vodUrl)/hls/manifest.m3u8"
+        } else {
+            return "\(liveUrl).m3u8"
+        }
+    }
+
+    public var sessionTokenUrl: String {
+        if videoType == .vod {
+            return "\(vodUrl)/session"
+        } else {
+            return hlsManifestUrl
+        }
+    }
+
+    public var mp4Url: String {
+        return "\(vodUrl)/mp4/source.mp4"
+    }
+
+    public var thumbnailUrl: String {
+        return "\(vodUrl)/thumbnail.jpg"
+    }
+
 }
 
 public enum VideoType: String {
-    case vod
-    case live
+    case vod = "https://vod.api.video/vod"
+    case live = "https://live.api.video"
 }
