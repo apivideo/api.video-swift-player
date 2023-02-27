5825c79c2b5fb6c168d79469ae9a8dfddde9c5ac
Romain Petit <contact.romain.petit@gmail.com>
2023-02-27 15:47:18 +0100

    feat() add PlayerItemFactory, and UrlFactory

---
+++ Sources/ApiVideoPlayer/ApiVideoPlayerItemFactory.swift
@@ -0,0 +1,63 @@
+import AVFoundation
+import Foundation
+class ApiVideoPlayerItemFactory {
+    private let videoOptions: VideoOptions
+    weak var delegate: ApiVideoPlayerItemFactoryDelegate?
+    private var urlFactory: ApiVideoUrlFactory!
+
+    init(videoOptions: VideoOptions) {
+        self.videoOptions = videoOptions
+        self.urlFactory = ApiVideoUrlFactory(videoOptions: videoOptions)
+        self.urlFactory.delegate = self
+    }
+
+    func getHlsPlayerItem(completion: @escaping (AVPlayerItem) -> Void) {
+        urlFactory.getHlsUrl { url in
+            guard let item = self.createPlayerItem(url: url) else {
+                return
+            }
+            completion(item)
+        }
+    }
+
+    func getMp4PlayerItem(completion: @escaping (AVPlayerItem) -> Void) {
+        urlFactory.getMp4Url { url in
+            guard let item = self.createPlayerItem(url: url) else {
+                return
+            }
+            completion(item)
+        }
+    }
+
+    func getThumbnailPlayerItem(completion: @escaping (AVPlayerItem) -> Void) {
+        urlFactory.getThumbnail { url in
+            guard let item = self.createPlayerItem(url: url) else {
+                return
+            }
+            completion(item)
+        }
+    }
+
+    private func createPlayerItem(url: String) -> AVPlayerItem? {
+        guard let path = URL(string: url) else {
+            delegate?.didError(PlayerError.urlError("Couldn't set up AVPlayerItem with invalid url"))
+            return nil
+        }
+        let item = AVPlayerItem(url: path)
+        return item
+    }
+
+}
+
+// MARK: ApiVideoUrlFactoryDelegate
+
+extension ApiVideoPlayerItemFactory: ApiVideoUrlFactoryDelegate {
+    func didError(_ error: Error) {
+        self.delegate?.didError(error)
+    }
+
+}
+
+protocol ApiVideoPlayerItemFactoryDelegate: AnyObject {
+    func didError(_ error: Error)
+}
