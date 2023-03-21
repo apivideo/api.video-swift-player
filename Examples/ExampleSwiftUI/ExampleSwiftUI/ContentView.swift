9f2132e4b0eb9fae69fc009c99bfaa8a11561703
Romain Petit <contact.romain.petit@gmail.com>
2023-03-21 15:48:26 +0100

    fix(swiftui) replace '=' by ':' init method

--- Examples/ExampleSwiftUI/ExampleSwiftUI/ContentView.swift
+++ Examples/ExampleSwiftUI/ExampleSwiftUI/ContentView.swift
@@ -28,7 +28,7 @@ struct ContentView: View {
             }
         )
         self.player = ApiVideoPlayer(
-            videoOptions: VideoOptions(videoId: "YOUR-VIDEO-ID", videoType = .vod),
+            videoOptions: VideoOptions(videoId: "YOUR-VIDEO-ID", videoType: .vod),
             events: events
         )
     }
