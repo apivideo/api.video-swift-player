ce04e8c4c319497f3b45ae984de9c19276cbc273
Romain Petit <contact.romain.petit@gmail.com>
2023-04-25 12:06:35 +0200

    fix() replace var to let and remove optional

--- Sources/ApiVideoPlayer/Models/NowPlayingData.swift
+++ Sources/ApiVideoPlayer/Models/NowPlayingData.swift
@@ -2,9 +2,17 @@ import CoreMedia
 import Foundation
 
 struct NowPlayingData {
-    var duration: CMTime?
-    var currentTime: CMTime?
-    var thumbnailUrl: String?
-    var isLive: Bool?
-    var title: String?
+    let duration: CMTime
+    let currentTime: CMTime
+    let isLive: Bool
+    let thumbnailUrl: String?
+    let title: String?
+
+    init(duration: CMTime, currentTime: CMTime, isLive: Bool, thumbnailUrl: String?, title: String? = nil) {
+        self.duration = duration
+        self.currentTime = currentTime
+        self.isLive = isLive
+        self.thumbnailUrl = thumbnailUrl
+        self.title = title
+    }
 }
