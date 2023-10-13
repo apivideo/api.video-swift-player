9c6fbd256de2122eafb883e2ddc0fba64a5d6d42
ThibaultBee <37510686+ThibaultBee@users.noreply.github.com>
2023-10-13 12:34:23 +0200

    chore(*): move to swift-client instead of ios-client

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apivideo/api.video-swift-player-analytics", from: "1.1.1"),
-        .package(url: "https://github.com/apivideo/api.video-ios-client", from: "1.2.1")
+        .package(url: "https://github.com/apivideo/api.video-swift-client", from: "1.2.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -33,7 +33,7 @@ let package = Package(
             name: "ApiVideoPlayerTests",
             dependencies: [
                 "ApiVideoPlayer",
-                .product(name: "ApiVideoClient", package: "api.video-ios-client")
+                .product(name: "ApiVideoClient", package: "api.video-swift-client")
             ],
             path: "Tests/ApiVideoPlayerTests",
             resources: [
